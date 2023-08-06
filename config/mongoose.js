d8f84849a1c0304146e31eaedab8ce9061108398
Tushar24042002 <sahilgupta24042002@gmail.com>
2023-08-06 11:48:51 +0530

    update

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 
 //connect to DB
 // const dbURL = 'mongodb://localhost/hospital_API';
-const dbURL = `mongodb+srv://tushargupta24042002:<password>@hospital-api.cvallai.mongodb.net/hospital-api?retryWrites=true&w=majority`;
-mongoose.connect('', { useNewUrlParser: true ,useUnifiedTopology: true });
+const dbURL = `mongodb+srv://tushargupta24042002:Tushar2002@hospital-api.cvallai.mongodb.net/hospital-api?retryWrites=true&w=majority`;
+mongoose.connect(dbURL, { useNewUrlParser: true ,useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "Error connecting to db"));
